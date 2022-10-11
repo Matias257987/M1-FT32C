1ab233db43b560c6216a5395609112afbb62ac3b
Matias <acostamatias25@gmail.com>
2022-10-11 15:28:00 -0300

    BinariToDecimal&&DecimalToBinary

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let numDec = 0;
 
-function DecimalABinario(num) {}
+   for (let i = 0; i < num.length; i++) {
+      numDec += num[i] * (2 ** (num.length - 1 - i));
+   }
+
+   return numDec;
+}
+
+function DecimalABinario(num) {
+   let arr = [];
+
+   while (num !== 0) {
+      arr.unshift(num % 2);
+      num = Math.floor(num / 2);
+   }
+
+   return arr.join('');
+}
 
 module.exports = {
    BinarioADecimal,
