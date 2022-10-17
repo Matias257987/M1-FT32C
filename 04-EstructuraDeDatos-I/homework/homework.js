4d1b8961d5ee6e5f1b289cd5bd1670d706418e6a
Matias <acostamatias25@gmail.com>
2022-10-17 10:38:28 -0300

    Recursion & Class (Queue)

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,13 +1,39 @@
 'use strict';
 
 // EJERCICIO 1
-function nFactorial(n) {}
+function nFactorial(n) {
+   if (n > -1 && n < 2) return 1;
+   else if (n < 0) return 0;
+
+   return n * nFactorial(n - 1);
+}
 
 // EJERCICIO 2
-function nFibonacci(n) {}
+function nFibonacci(n) {
+   if (n === 0 || n === 1) return n;
+   else if (n < 0) return "ERROR";
+
+   return nFibonacci(n - 1) + nFibonacci(n - 2);
+}
 
 // EJERCICIO 3
-function Queue() {}
+class Queue {
+   constructor() {
+      this.array = [];
+   }
+   enqueue(value) {
+      this.array.push(value);
+   }
+   dequeue() {
+      return this.array.shift();
+   }
+   size() {
+      return this.array.length;
+   }
+}
+
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
