8fabbfc0f326ef7e54e739a8991cd509b97ba5a4
Matias <acostamatias25@gmail.com>
2022-10-20 14:28:45 -0300

    QuickSort & MergeSort

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,18 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length < 2) return array;
 
+  let pivot = array[0];
+  let min = [];
+  let max = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] <= pivot) min.push(array[i]);
+    else max.push(array[i]);
+  }
+
+  return quickSort(min).concat(pivot).concat(quickSort(max));
 }
 
 function mergeSort(array) {
@@ -14,7 +25,22 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length < 2) return array;
 
+  let middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle);
+  let result = [];
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+
+  while (left.length && right.length) {
+    if (left[0] > right[0]) result.push(right.shift());
+    else result.push(left.shift());
+  }
+  
+  return result.concat(left).concat(right);
   //okey probemos ahora
 }
 
